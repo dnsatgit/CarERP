1bba487a4a46769629e45cc3f7ca064aef35b9a5
Dorje Sherpa <d_sherpa@salemstate.edu>
2018-04-27 22:03:01 -0400

    ViewBag, ViewData and tempData
    AddCustomer model was initiated; three view methods were addressed, also, we instanciated new variables in the model, then created a controller for those variables to run on the server. Finally, view was edited as per requirement

--- CarERP/CarERP/Controllers/HomeController.cs
+++ CarERP/CarERP/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CarERP.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,34 @@ namespace CarERP.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: Home
         public ActionResult Index()
         {
-            return View();
+            ViewBag.product = "Customer";
+
+            var model = new AddCustomerViewModel()
+            {
+                first_name = "Dorje",
+                last_name = "Sherpa",
+                phone = 6171111111,
+                address = "1000 Loring ave., Salem, MA 00000",
+                email = "d_sherpa@salemstate.edu",
+                id = 0309438,
+
+            };
+            return View(model);
+        }
+
+        [HttpGet]
+        public ActionResult addCustomer()
+        {
+            return View(new AddCustomerViewModel());
+        }
+        [HttpPost]
+        public ActionResult addCustomer(AddCustomerViewModel model)
+        {
+            if (ModelState.IsValid)
+                ViewBag.result = "Customer has been added sucessfully.";
+            return View(new AddCustomerViewModel());
         }
     }
 }
